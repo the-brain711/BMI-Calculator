39e7b11298f720c33ae5e8fd57230265ee14dc46
Brian Nguyen <briannguyen9101@gmail.com>
2024-03-07 20:22:20 -0600

    Define requirements for program

---
+++ main.py
@@ -0,0 +1,30 @@
+class UserInput:
+    def __init__(self) -> None:
+        self.feet = None
+        self.inches = None
+        self.weight = None
+
+        self._getUserInput()
+
+    def _getUserInput(self) -> None:
+        pass
+
+
+class BMICalculator:
+    def calculate(height: int, weight: int) -> float:
+        pass
+
+    def categorize(bmi: float) -> str:
+        pass
+
+
+def calculateTotalInches(feet: int, inches: int) -> int:
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()

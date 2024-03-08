bc91843f339cf55d0404e0d18a55ba0e09d92968
Brian Nguyen <briannguyen9101@gmail.com>
2024-03-07 23:45:52 -0600

    Update name for test case

--- unit_test.py
+++ unit_test.py
@@ -5,16 +5,16 @@ from main import *
 
 class TestCalculateTotalInches(unittest.TestCase):
     def test_valid_inputs(self):
-        self.assertEqual(calculateTotalInches(5, 9), 69)
+        self.assertEqual(calculate_total_inches(5, 9), 69)
 
     def test_negative_feet(self):
-        self.assertRaises(ValueError, calculateTotalInches, -5, 9)
+        self.assertRaises(ValueError, calculate_total_inches, -5, 9)
 
     def test_negative_inches(self):
-        self.assertRaises(ValueError, calculateTotalInches, 5, -7)
+        self.assertRaises(ValueError, calculate_total_inches, 5, -7)
 
     def test_negative_inputs(self):
-        self.assertRaises(ValueError, calculateTotalInches, -5, -10)
+        self.assertRaises(ValueError, calculate_total_inches, -5, -10)
 
 
 class TestUserInput(unittest.TestCase):
